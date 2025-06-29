e65c6cedaac0264760c9f3e37a6c487f5f0b2cfb
Behruz <BehruzzCoder@gmail.com>
2025-06-30 00:56:29 +0500

    done

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,10 @@
 import Major from "./Major";
+import MajorCreate from "./MajorCreate";
+import MajorMore from "./MajorMore";
 import Groups from "./Groups";
 import Students from "./Students";
 import Teachers from "./Teachers";
 import Home from "./Home";
-export {Major, Groups, Students, Teachers, Home}
+import GroupCreate from "./GroupCreate";
+
+export {Major, Groups, Students, Teachers, Home, MajorCreate, MajorMore, GroupCreate}
--- src/types/DashboardRouteType.tsx
+++
@@ -1,7 +0,0 @@
-import type { ReactNode } from "react";
-
-export interface DashboardRouteType {
-  id: number;
-  path: string;
-  element: ReactNode;
-}
